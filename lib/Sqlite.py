37d511753b61c3f0d9fa08efc497e177d2b462c7
Matthijs Zondervan <mzondervan@hotmail.nl>
2018-12-07 15:31:38 +0100

    Added basic functions
    all function based on the class diagram

---
+++ lib/Sqlite.py
@@ -0,0 +1,27 @@
+import sqlite3
+conn = sqlite3.connect('database.db')
+c = conn.cursor()
+
+def setUpDataBase:
+
+def checkDataBase:
+
+def setLogItem(dateTime, title, details):
+
+def getLogItemDetails(logId, title):
+
+def getLogItems(args[]):
+
+def setCase(caseId, date):
+
+def getCase(caseId, fields):
+
+def getCases(args[]):
+
+def setEvidenseItem(hashes[], file, extention, size, dateTime):
+
+def getEvidenseItemDetails(evidenseId, fields):
+
+def getEvidenseItems(args[]):
+
+def logItem(file, location, tag):
