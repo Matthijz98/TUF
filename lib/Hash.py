750fb97d7a5aefc55ee5024ae1abfaa7e46b4c0f
Dylan <dyland66@hotmail.nl>
2019-01-09 11:13:36 +0100

    MD5 efficiënter gemaakt

--- lib/Hash.py
+++ lib/Hash.py
@@ -3,13 +3,21 @@ import hashlib
 
 def hashing(filename):
 
+    # buffer size instellen zodat grotere bestanden sneller worden gelezen
+    buffer_size = 65536
     hash = hashlib.md5()
 
+    # bestand openen
     with open(filename, 'rb') as file:
-        buffer = file.read()
-        hash.update(buffer)
+        while True:
+            buffer = file.read(buffer_size)
+            if not buffer:
+                break
+            hash.update(buffer)
     return hash.hexdigest()
 
 
-value = hashing("Voer hier het pad in")
-print(value)
+# file path aangeven en de md5 printen
+value = hashing("")
+print("De MD5-Hash:", value)
+
