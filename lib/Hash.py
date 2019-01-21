5bf3477c5f6907753715a20283394d526b92fcde
Dylan <dyland66@hotmail.nl>
2019-01-21 08:50:14 +0100

    Commentaar toegevoegd

--- lib/Hash.py
+++ lib/Hash.py
@@ -1,19 +1,24 @@
+# Hash library importeren
 import hashlib
 
 
+# Functie hashing aanmaken met een parameter voor het bestand
 def hashing(filename):
 
     # buffer size instellen zodat grotere bestanden sneller worden gelezen
     buffer_size = 65536
+    # sha1 hash ophalen uit de library
     hash = hashlib.sha1()
 
     # bestand openen
     with open(filename, 'rb') as file:
         while True:
+            # bestand wordt gelezen met de meegegeven buffer_size
             buffer = file.read(buffer_size)
             if not buffer:
                 break
             hash.update(buffer)
+    # hash waarde wordt terugeggeven
     return hash.hexdigest()
 
 
