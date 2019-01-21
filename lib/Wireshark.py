344fbb4e4faac5188dfb9932cc482e184ba49c3d
Dylan <dyland66@hotmail.nl>
2019-01-21 11:26:54 +0100

    Try except toegevoegd

--- lib/Wireshark.py
+++ lib/Wireshark.py
@@ -5,14 +5,20 @@ Studentnummer:
 # Van de library scapy alles importeren
 from scapy.all import *
 
-# rdpcap importeren van scapy. Dit laadt de file in.
-# voer hier het pad naar de file in
-packets = rdpcap('')
+# Een try om te kijken of het wel een .pcap bestand is
+try:
+    # rdpcap importeren van scapy. Dit laadt de file in.
+    # voer hier het pad naar de file in
+    packets = rdpcap('')
 
-# Hier worden het aantal packets getoond
-print("Aantal packets:", len(packets))
+    # Hier worden het aantal packets getoond
+    print("Aantal packets:", len(packets))
 
-# Hier wordt elke packet getoond
-for packet in packets:
-    packets.show()
-    break
+    # Hier wordt elke packet getoond
+    for packet in packets:
+        packets.show()
+        break
+
+# Dit vangt de error op om vervolgens een melding terug te geven
+except Scapy_Exception:
+    print("Dit is geen Wireshark bestand.")
