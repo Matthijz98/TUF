2cba3216f271c3b0b49d06e8dc9f09b1dfb3c660
Dylan <dyland66@hotmail.nl>
2019-01-21 08:54:40 +0100

    Commentaart toegevoegd

--- lib/Wireshark.py
+++ lib/Wireshark.py
@@ -1,13 +1,18 @@
+"""
+Gemaakt door:
+Studentnummer:
+"""
+# Van de library scapy alles importeren
 from scapy.all import *
 
 # rdpcap importeren van scapy. Dit laadt de file in.
 # voer hier het pad naar de file in
 packets = rdpcap('')
 
+# Hier worden het aantal packets getoond
 print("Aantal packets:", len(packets))
 
-# elke packet wordt getoond
+# Hier wordt elke packet getoond
 for packet in packets:
     packets.show()
     break
-    #print(packets)
