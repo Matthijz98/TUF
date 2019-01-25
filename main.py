17fa6fb209a80b499ec474597057c49a6c50c215
Oskar <sokka11@interia.pl>
2019-01-25 10:23:40 +0100

    Kan beter

--- main.py
+++ main.py
@@ -3,12 +3,13 @@ if __name__ == '__main__':
     # from lib import Settings
     from lib import Sqlite
     import os
+    import PySimpleGUI as Sg
 
     path = os.path.dirname(os.path.realpath(__file__))
     print(path)
     name = 'database'
     db = ''
-    ingelogt = False
+    loggedin = False
     # the currently open case
     active_case = ''
 
@@ -17,8 +18,7 @@ if __name__ == '__main__':
         values = {"number": number, "title": title, "note": note}
         db.set_case(values)
 
-    import PySimpleGUI as Sg
-
+# The layout of the window is created with the 'layout' variable
     layout = [[Sg.Text('At which location would you like to save the TUF database?')],
               [Sg.Input(path), Sg.FolderBrowse()],
               [Sg.T(' ' * 25), Sg.Button('Yes'), Sg.Button('No')]]
@@ -28,62 +28,68 @@ if __name__ == '__main__':
 
     while True:
         ev1, vals1 = win1.Read()
-
+# If the 'close' button is pushed the program is going to stop with running
         if ev1 is None:
             break
-
+# If the 'No' button is pushed the program is going to stop with running
         if ev1 == 'No':
             break
-
+# If the 'Yes' button is pushed the program is going to save the database in the location you've chose
         if ev1 == 'Yes':
             if path != vals1[0]:
                 path = vals1[0]
             db = Sqlite.Sqlite(path, name)
             db.setup_database()
-
+# If the 'Yes' button is pushed the program is going to activate the 2nd window and hide the previous window.
         if not win2_active and ev1 == 'Yes':
             win2_active = True
             win1.Hide()
+# The layout of the window is created with the 'layout' variable
             layout2 = [[Sg. Text('Login as a TUF user or create a new user account.')],
                        [Sg.Text('Username'), Sg.Input()],
-                       [Sg.Text('Password'), Sg.Input()],
+                       [Sg.Text('Password'), Sg.Input(password_char='*')],
                        [Sg.Button('Login'), Sg.Button('Create User')]]
             win2 = Sg.Window('TUF - Turtle Forensics', size=(400, 300), icon='ICON.ico').Layout(layout2)
 
         if win2_active:
             ev2, vals2 = win2.Read()
 
-            while ingelogt is False:
-
+            while loggedin is False:
+                # If the 'Login' button is pushed the program is going check if the username and password
+                # exist in the database.
                 if ev2 == 'Login':
                     username = vals2[0]
                     password = vals2[1]
-                    check_ingelogt = db.check_user(username, password)
-
-                    if check_ingelogt is True:
-                        ingelogt = True
-                        Sg.Popup("te bent ingelogt")
+                    check_loggedin = db.check_user(username, password)
+# If the username and password are correct then the program is going show a popup screen and go to the next screen.
+                    if check_loggedin is True:
+                        loggedin = True
+                        Sg.Popup("You've been logged in successfully! Welcome " + vals2[0] + "!")
                         break
-                    elif check_ingelogt is False:
-                        ingelogt = False
-                        Sg.Popup("deze combinatie van gebruikers naam en wachtwoord bestaat niet")
+# If the username and password are incorrect then the program is going show a popup screen and stay on the same window.
+                    elif check_loggedin is False:
+                        loggedin = False
+                        Sg.Popup("This combination of username and password does not exist.")
                         ev2, vals2 = win2.Read()
+
                     else:
-                        Sg.Popup("er is iets fout gegaan")
-                        ingelogt = False
+                        Sg.Popup("Something went wrong. Try again.")
+                        loggedin = False
+
                 if ev2 == 'Create User':
                     values = [vals2[0], vals2[1]]
                     db.set_user(values)
-                    Sg.Popup("Een user met de gebruikersnaam " + vals2[0] + " gemaakt")
-                    ingelogt = True
-                    break
+                    Sg.Popup("The user with the name " + vals2[0] + " has been created!")
+                    loggedin = True
+
+                elif check_loggedin is False:
+                    check_loggedin = db.check_user(username, password)
+                    Sg.Popup("The user with the name " + vals2[0] + " already exists!")
+
                 if ev2 is None:
                     break
 
-
-
-
-        if not win3_active and ingelogt is True:
+        if not win3_active and ev2 == 'Login' and loggedin is True:
             cases = db.get_cases()
             table = Sg.Table(cases,
                              headings=["case", "case id", "title", "description", "date created"],
@@ -99,19 +105,19 @@ if __name__ == '__main__':
 
             while True:
                 if win3_active:
-
                     ev3, vals3 = win3.Read()
 
                     if ev3 == 'clicked':
-
                         Sg.Popup(vals3)
+
                     if ev3 == 'Open Case':
                         active_case = cases[vals3[0][0]][0]
                         break
+
                     if ev3 is None:
                         break
 
-        if not win5_active and ev3 == 'Create Case' and ingelogt is True:
+        if not win5_active and ev3 == 'Create Case' and loggedin is True:
             win5_active = True
             win3.Hide()
             layout5 = [[Sg.Text('Case Number: '), Sg.Input()],
@@ -120,7 +126,7 @@ if __name__ == '__main__':
                        [Sg.Button('Back'), Sg.Button('Save Case')]]
             win5 = Sg.Window('Turtle Forensics - Create Case', icon='ICON.ico').Layout(layout5)
 
-        if win5_active and ingelogt is True:
+        if win5_active and loggedin is True:
             ev5, vals5 = win5.Read()
             if ev5 == 'Save Case':
                 x = ev5, vals5[0]
@@ -147,12 +153,12 @@ if __name__ == '__main__':
 
                 if ev6 == 'Save':
                     if vals6['e01']:
-                        print('Hello mijn nama mama')
+                        print('e01')
                     if vals6['RAW']:
                         print('raw')
 
-        # case overvieuw
-        if not win6_active and ev3 == 'Open Case' and ingelogt is True:
+        # case overview
+        if not win6_active and ev3 == 'Open Case' and loggedin is True:
             case = db.get_case(1)
             win3.Hide()
             print(case)
