28599ebeff1cce430f0213f29cc4009e8a0da73b
G1do11 <s1108069@student.hsleiden.nl>
2019-02-01 01:12:52 +0100

    YEEY WERKT

--- lib/Image.py
+++ lib/Image.py
@@ -1,3 +1,9 @@
+#######################
+#       Made By       #
+# Gido Scherpenhuizen #
+#      S1108069       #
+#######################
+
 import datetime
 import hashlib
 from os.path import join as pjoin
@@ -6,7 +12,6 @@ import lib.Sqlite
 
 import pyewf
 import pytsk3
-from tabulate import tabulate
 
 
 # Create class to make pytsk3.Img_Info a pyewf function
@@ -45,10 +50,43 @@ class ewf_Img_Info(pytsk3.Img_Info):
             outfile.write(filedata)
 """
 
+#db = lib.Sqlite.Sqlite(path=r"C:\Users\Gido Scherpenhuizen\Documents\,School\IIPFIT5\Project Files", filename="test")
+#db.setup_database()
 
-class test():
+
+def addtodb(db, partition_id, parent_key,  md5_hash, sha256_hash, sha1_hash, name, create, modify, filepath, size, extension, f_type):
+    db.set_file(partition_id, parent_key,  md5_hash, sha256_hash, sha1_hash, name, create, modify, filepath, size, extension, f_type)
+
+
+def getdirectorydata(db, change_dir, parent_key):
+    for f in test.main("ImageUSBSjors.dd.001", "raw", change_dir):
+        if f[10] == "DIR":
+            changedir = change_dir
+            if f[4] != "." and f[4] != "..":
+                print(f[4])
+                addtodb(db, f[0], parent_key, f[1], f[2], f[3], f[4], f[5], f[6], f[7], f[8], f[9], f[10])
+                changedir = change_dir + "/" + f[4]
+                p_key = f[4]
+                getdirectorydata(db, changedir, p_key)
+        else:
+            addtodb(db, f[0], parent_key, f[1], f[2], f[3], f[4], f[5], f[6], f[7], f[8], f[9], f[10])
+
+
+def start(db):
+    for f in test.main("ImageUSBSjors.dd.001", "raw"):
+        if f[10] == "DIR":
+            if f[4] != "." and f[4] != "..":
+                addtodb(db, f[0], "", f[1], f[2], f[3], f[4], f[5], f[6], f[7], f[8], f[9], f[10])
+                change_dir = f[4]
+                parent_key = f[4]
+                getdirectorydata(db, change_dir, parent_key)
+        else:
+            addtodb(db, f[0], "", f[1], f[2], f[3], f[4], f[5], f[6], f[7], f[8], f[9], f[10])
+
+
+class test:
     def main(imagefile, imagetype, change_dir=None):
-        if (imagetype == "e01"):  # Vervangen door return uit de GUI
+        if imagetype == 'e01':  # Vervangen door return uit de GUI
             filenames = pyewf.glob(imagefile)
             ewf_handle = pyewf.handle()
             ewf_handle.open(filenames)
@@ -61,6 +99,7 @@ class test():
         # test.printpartitiontable(imagehandle, volume)
         # test.listfiles(volume, imagehandle)
 
+        """
         for partition in volume:
             print(partition.addr, partition.desc.decode('utf-8'), "%ss(%s)" % (partition.start, partition.start * 512),
                 partition.len)
@@ -70,25 +109,24 @@ class test():
                 print("Partition has no supported file system")
                 continue
             print("File System Type Dectected ", filesystemObject.info.ftype)
+        """
 
         for partition in volume:
-            # Variabele return vanuit de GUI met gekozen partitie
-            if 'FAT16' in partition.desc.decode('utf-8'):
+            if 'NTFS' in partition.desc.decode('utf-8') or 'FAT16' in partition.desc.decode('utf-8') \
+                    or 'FAT32' in partition.desc.decode('utf-8') or 'EXT2' in partition.desc.decode('utf-8') \
+                    or 'EXT3' in partition.desc.decode('utf-8') or 'EXT4' in partition.desc.decode('utf-8'):
                 partition_id = partition.addr
                 filesystemObject = pytsk3.FS_Info(imagehandle, offset=partition.start * 512)
 
-                current_dir = ""
+                root_dir = ""
+                current_dir = str(change_dir)
                 if change_dir is None:
-                    open_current_dir = filesystemObject.open_dir(path=current_dir)
+                    open_current_dir = filesystemObject.open_dir(path=root_dir)
                 else:
-                    current_dir = current_dir + change_dir
                     open_current_dir = filesystemObject.open_dir(path=current_dir)
 
-                # Only for test purpose
-                table = [["Name", "Type", "Size", "Create Date", "Modify Date"]]
-
-                # Functie van maken om aan te roepen vanuit de gui
                 filelist = []
+
                 for f in open_current_dir:
                     name = f.info.name.name.decode('utf-8')
                     if hasattr(f.info.meta, 'type'):
@@ -99,16 +137,15 @@ class test():
                             f_type = "FILE"
                             if "." in name:
                                 extension = name.rsplit(".")[-1].lower()
-
                     size = f.info.meta.size
                     filepath = pjoin(current_dir, "/", f.info.name.name.decode('utf-8'))
                     create = datetime.datetime.fromtimestamp(f.info.meta.crtime).strftime('%Y-%m-%d %H:%M:%S')
                     modify = datetime.datetime.fromtimestamp(f.info.meta.mtime).strftime('%Y-%m-%d %H:%M:%S')
 
                     #if f_type == "FILE":
-                    print(pjoin("/", filepath))
-                      #  fileobject = filesystemObject.open(pjoin(filepath))
-                       # # completeName = pjoin(filepath)
+                        # print(pjoin("/", filepath))
+                        # fileobject = filesystemObject.open(pjoin(filepath))
+                        # completeName = pjoin(filepath)
                         #outfile = open(pjoin('C:/Users/Gido Scherpenhuizen/Documents/,School/OUTPUT/' + f.info.name.name.decode('utf-8')), 'wb')
                         #filedata = fileobject.read_random(0, fileobject.info.meta.size)
                         #outfile.write(filedata)
@@ -129,13 +166,11 @@ class test():
                     md5_hash = ""
                     sha256_hash = ""
                     sha1_hash = ""
-                    filelist.append([partition_id, md5_hash, sha256_hash, sha1_hash, name, create, modify, filepath, size, extension, f_type])
 
-                db = lib.Sqlite.Sqlite(path=r"C:\Users\Gido Scherpenhuizen\Documents\,School\IIPFIT5\Project Files", filename="test")
-                db.setup_database()
-                db.set_files(filelist)
-
+                    filelist.append([partition_id, md5_hash, sha256_hash, sha1_hash, name, create, modify, filepath,
+                                     size, extension, f_type])
                 return filelist
+
 
 """
                 for f in current_dir:
