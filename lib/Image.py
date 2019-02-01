0050c5c709e6fbe0eae682fb99d606c6a98caef6
G1do11 <s1108069@student.hsleiden.nl>
2019-02-01 05:54:34 +0100

    YEEY WERKT

--- lib/Image.py
+++ lib/Image.py
@@ -4,12 +4,11 @@
 #      S1108069       #
 #######################
 
+# Import libraries used for time conversion and easy path joins
 import datetime
-import hashlib
 from os.path import join as pjoin
 
-import lib.Sqlite
-
+# import libraries used to open an E01 and RAW image file
 import pyewf
 import pytsk3
 
@@ -31,39 +30,17 @@ class ewf_Img_Info(pytsk3.Img_Info):
         return self._ewf_handle.get_media_size()
 
 
-"""
-            FILE EXPORT !!FUNCTIE VAN MAKEN!!
-            
-            # Pick a file
-            fileObject = filesystemObject.open(pjoin(r'/Windows/System32/winevt/Logs/HardwareEvents.evtx'))
-            fileName = str(fileObject.info.name.name.decode('utf-8'))
-
-            # Print info from file
-            print("File Inode:", fileObject.info.meta.addr)
-            print("File Name", fileName)
-            print("File Creation Time", datetime.datetime.fromtimestamp(fileObject.info.meta.crtime).strftime('%Y-%m-%d %H:%M:%S'))
-
-            # Output file
-            completeName = pjoin(, fileName)
-            outfile = open(completeName, 'wb')
-            filedata = fileObject.read_random(0, fileObject.info.meta.size)
-            outfile.write(filedata)
-"""
-
-#db = lib.Sqlite.Sqlite(path=r"C:\Users\Gido Scherpenhuizen\Documents\,School\IIPFIT5\Project Files", filename="test")
-#db.setup_database()
-
-
+# Function to add data to the database
 def addtodb(db, partition_id, parent_key,  md5_hash, sha256_hash, sha1_hash, name, create, modify, filepath, size, extension, f_type):
     db.set_file(partition_id, parent_key,  md5_hash, sha256_hash, sha1_hash, name, create, modify, filepath, size, extension, f_type)
 
 
+# Function to retreive data from a directory
 def getdirectorydata(db, change_dir, parent_key):
     for f in test.main("ImageUSBSjors.dd.001", "raw", change_dir):
         if f[10] == "DIR":
             changedir = change_dir
             if f[4] != "." and f[4] != "..":
-                print(f[4])
                 addtodb(db, f[0], parent_key, f[1], f[2], f[3], f[4], f[5], f[6], f[7], f[8], f[9], f[10])
                 changedir = change_dir + "/" + f[4]
                 p_key = f[4]
@@ -72,6 +49,7 @@ def getdirectorydata(db, change_dir, parent_key):
             addtodb(db, f[0], parent_key, f[1], f[2], f[3], f[4], f[5], f[6], f[7], f[8], f[9], f[10])
 
 
+# Function where the file and folder extraction starts
 def start(db):
     for f in test.main("ImageUSBSjors.dd.001", "raw"):
         if f[10] == "DIR":
@@ -86,7 +64,7 @@ def start(db):
 
 class test:
     def main(imagefile, imagetype, change_dir=None):
-        if imagetype == 'e01':  # Vervangen door return uit de GUI
+        if imagetype == 'e01':
             filenames = pyewf.glob(imagefile)
             ewf_handle = pyewf.handle()
             ewf_handle.open(filenames)
@@ -96,21 +74,8 @@ class test:
         else:
             imagehandle = pytsk3.Img_Info(imagefile)
         volume = pytsk3.Volume_Info(imagehandle)
-        # test.printpartitiontable(imagehandle, volume)
-        # test.listfiles(volume, imagehandle)
 
-        """
-        for partition in volume:
-            print(partition.addr, partition.desc.decode('utf-8'), "%ss(%s)" % (partition.start, partition.start * 512),
-                partition.len)
-            try:
-                filesystemObject = pytsk3.FS_Info(imagehandle, offset=(partition.start * 512))
-            except:
-                print("Partition has no supported file system")
-                continue
-            print("File System Type Dectected ", filesystemObject.info.ftype)
-        """
-
+        # Check partitions for partition type and open them
         for partition in volume:
             if 'NTFS' in partition.desc.decode('utf-8') or 'FAT16' in partition.desc.decode('utf-8') \
                     or 'FAT32' in partition.desc.decode('utf-8') or 'EXT2' in partition.desc.decode('utf-8') \
@@ -118,6 +83,7 @@ class test:
                 partition_id = partition.addr
                 filesystemObject = pytsk3.FS_Info(imagehandle, offset=partition.start * 512)
 
+                # Open directory and change directory
                 root_dir = ""
                 current_dir = str(change_dir)
                 if change_dir is None:
@@ -125,8 +91,10 @@ class test:
                 else:
                     open_current_dir = filesystemObject.open_dir(path=current_dir)
 
+                # Make a list where all file data will be stored
                 filelist = []
 
+                # Open and check every file for data
                 for f in open_current_dir:
                     name = f.info.name.name.decode('utf-8')
                     if hasattr(f.info.meta, 'type'):
@@ -142,49 +110,11 @@ class test:
                     create = datetime.datetime.fromtimestamp(f.info.meta.crtime).strftime('%Y-%m-%d %H:%M:%S')
                     modify = datetime.datetime.fromtimestamp(f.info.meta.mtime).strftime('%Y-%m-%d %H:%M:%S')
 
-                    #if f_type == "FILE":
-                        # print(pjoin("/", filepath))
-                        # fileobject = filesystemObject.open(pjoin(filepath))
-                        # completeName = pjoin(filepath)
-                        #outfile = open(pjoin('C:/Users/Gido Scherpenhuizen/Documents/,School/OUTPUT/' + f.info.name.name.decode('utf-8')), 'wb')
-                        #filedata = fileobject.read_random(0, fileobject.info.meta.size)
-                        #outfile.write(filedata)
-                        #outfile.close()
-                        # f_size = getattr(f.info.meta, "size", 0)
-                        # md5_hash = hashlib.md5()  # MD5 FUNCTIE VAN DYLAN
-                        # md5_hash.update(filedata)
-                        # print(md5_hash.hexdigest)
-
-                        # sha256_hash = hashlib.sha256()
-                        # sha256_hash.update(f.read_random(outfile))
-                        # sha256_hash.hexdigest
-
-                        # sha1_hash = hashlib.sha1()
-                        # sha1_hash.update(f.read_random(outfile))
-                        # sha1_hash.hexdigest
-
                     md5_hash = ""
                     sha256_hash = ""
                     sha1_hash = ""
 
                     filelist.append([partition_id, md5_hash, sha256_hash, sha1_hash, name, create, modify, filepath,
                                      size, extension, f_type])
+                # Return the list of file data
                 return filelist
-
-
-"""
-                for f in current_dir:
-                    name = f.info.name.name
-                    if hasattr(f.info.meta, 'type'):
-                        if f.info.meta.type == pytsk3.TSK_FS_META_TYPE_DIR:
-                            f_type = "DIR"
-                        else:
-                            f_type = "FILE"
-                        size = f.info.meta.size
-                        create = datetime.datetime.fromtimestamp(f.info.meta.crtime).strftime('%Y-%m-%d %H:%M:%S')
-                        modify = datetime.datetime.fromtimestamp(f.info.meta.mtime).strftime('%Y-%m-%d %H:%M:%S')
-                        table.append([name, f_type, size, create, modify])
-                # Only for test purpose
-                print(tabulate(table, headers="firstrow"))
-
-"""
--- lib/Proef102.py
+++ lib/Proef102.py
@@ -1,71 +1,53 @@
 import lib.PySimpleGUI as Sg
 import lib.Image
 import lib.Sqlite
-from random import randint
 
 tree = Sg.TreeData()
 
 treegoed = Sg.Tree(data=tree,
-                   headings=['partition_id', 'file_md5', 'file_sha256', 'file_sha1', 'title', 'date_created', 'date_last_modified', 'file_path', 'size', 'extension', 'file_type'],
+                   headings=['partition_id', 'file_md5', 'file_sha256', 'file_sha1', 'title', 'date_created',
+                             'date_last_modified', 'file_path', 'size', 'extension', 'file_type'],
                    auto_size_columns=True,
                    right_click_menu=['&Right', ['Extract', 'Upload to VirusTotal']],
                    enable_events=True)
 
-folder_icon = b'iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAAACXBIWXMAAAsSAAALEgHS3X78AAABnUlEQVQ4y8WSv2rUQRSFv7vZgJFFsQg2EkWb4AvEJ8hqKVilSmFn3iNvIAp21oIW9haihBRKiqwElMVsIJjNrprsOr/5dyzml3UhEQIWHhjmcpn7zblw4B9lJ8Xag9mlmQb3AJzX3tOX8Tngzg349q7t5xcfzpKGhOFHnjx+9qLTzW8wsmFTL2Gzk7Y2O/k9kCbtwUZbV+Zvo8Md3PALrjoiqsKSR9ljpAJpwOsNtlfXfRvoNU8Arr/NsVo0ry5z4dZN5hoGqEzYDChBOoKwS/vSq0XW3y5NAI/uN1cvLqzQur4MCpBGEEd1PQDfQ74HYR+LfeQOAOYAmgAmbly+dgfid5CHPIKqC74L8RDyGPIYy7+QQjFWa7ICsQ8SpB/IfcJSDVMAJUwJkYDMNOEPIBxA/gnuMyYPijXAI3lMse7FGnIKsIuqrxgRSeXOoYZUCI8pIKW/OHA7kD2YYcpAKgM5ABXk4qSsdJaDOMCsgTIYAlL5TQFTyUIZDmev0N/bnwqnylEBQS45UKnHx/lUlFvA3fo+jwR8ALb47/oNma38cuqiJ9AAAAAASUVORK5CYII='
-file_icon = b'iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAAACXBIWXMAAAsSAAALEgHS3X78AAABU0lEQVQ4y52TzStEURiHn/ecc6XG54JSdlMkNhYWsiILS0lsJaUsLW2Mv8CfIDtr2VtbY4GUEvmIZnKbZsY977Uwt2HcyW1+dTZvt6fn9557BGB+aaNQKBR2ifkbgWR+cX13ubO1svz++niVTA1ArDHDg91UahHFsMxbKWycYsjze4muTsP64vT43v7hSf/A0FgdjQPQWAmco68nB+T+SFSqNUQgcIbN1bn8Z3RwvL22MAvcu8TACFgrpMVZ4aUYcn77BMDkxGgemAGOHIBXxRjBWZMKoCPA2h6qEUSRR2MF6GxUUMUaIUgBCNTnAcm3H2G5YQfgvccYIXAtDH7FoKq/AaqKlbrBj2trFVXfBPAea4SOIIsBeN9kkCwxsNkAqRWy7+B7Z00G3xVc2wZeMSI4S7sVYkSk5Z/4PyBWROqvox3A28PN2cjUwinQC9QyckKALxj4kv2auK0xAAAAAElFTkSuQmCC'
+folder_icon = b'iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAAACXBIWXMAAAsSAAALEgHS3X78AAABnUlEQVQ4y8WSv2rUQRSFv7vZ\
+gJFFsQg2EkWb4AvEJ8hqKVilSmFn3iNvIAp21oIW9haihBRKiqwElMVsIJjNrprsOr/5dyzml3UhEQIWHhjmcpn7zblw4B9lJ8Xag9mlmQb3AJzX3tOX8\
+Tngzg349q7t5xcfzpKGhOFHnjx+9qLTzW8wsmFTL2Gzk7Y2O/k9kCbtwUZbV+Zvo8Md3PALrjoiqsKSR9ljpAJpwOsNtlfXfRvoNU8Arr/NsVo0ry5z4dZ\
+N5hoGqEzYDChBOoKwS/vSq0XW3y5NAI/uN1cvLqzQur4MCpBGEEd1PQDfQ74HYR+LfeQOAOYAmgAmbly+dgfid5CHPIKqC74L8RDyGPIYy7+QQjFWa7ICs\
+Q8SpB/IfcJSDVMAJUwJkYDMNOEPIBxA/gnuMyYPijXAI3lMse7FGnIKsIuqrxgRSeXOoYZUCI8pIKW/OHA7kD2YYcpAKgM5ABXk4qSsdJaDOMCsgTIYAlL5\
+TQFTyUIZDmev0N/bnwqnylEBQS45UKnHx/lUlFvA3fo+jwR8ALb47/oNma38cuqiJ9AAAAAASUVORK5CYII='
 
-"""
-def getdirdata(change_dir, parent_key):
-    s = 0
-    for f in (lib.mount.test.main("micro-sd-drone.001", "raw", change_dir)):
-        if f[10] == "DIR":
-            if f[4] == "." or f[4] == "..":
-                tree.Insert(parent_key, f[4], str(s), f, icon=folder_icon)
-                s += 1
-                continue
-            else:
-                tree.Insert(parent_key, f[4], f[4], f, icon=folder_icon)
-                change_dir = f[7]
-                parent_key = f[4]
-                getdirdata(change_dir, parent_key)
-        else:
-            tree.Insert(parent_key, f[4], f[4], f, icon=file_icon)
+file_icon = b'iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAAACXBIWXMAAAsSAAALEgHS3X78AAABU0lEQVQ4y52TzStEURiHn/ecc6X\
+G54JSdlMkNhYWsiILS0lsJaUsLW2Mv8CfIDtr2VtbY4GUEvmIZnKbZsY977Uwt2HcyW1+dTZvt6fn9557BGB+aaNQKBR2ifkbgWR+cX13ubO1svz++niVT\
+A1ArDHDg91UahHFsMxbKWycYsjze4muTsP64vT43v7hSf/A0FgdjQPQWAmco68nB+T+SFSqNUQgcIbN1bn8Z3RwvL22MAvcu8TACFgrpMVZ4aUYcn77BMDk\
+xGgemAGOHIBXxRjBWZMKoCPA2h6qEUSRR2MF6GxUUMUaIUgBCNTnAcm3H2G5YQfgvccYIXAtDH7FoKq/AaqKlbrBj2trFVXfBPAea4SOIIsBeN9kkCwxsN\
+kAqRWy7+B7Z00G3xVc2wZeMSI4S7sVYkSk5Z/4PyBWROqvox3A28PN2cjUwinQC9QyckKALxj4kv2auK0xAAAAAElFTkSuQmCC'
 
 
-for f in (lib.mount.test.main("micro-sd-drone.001", "raw")):
-    if f[10] == "DIR":
-        if f[4] == "." or f[4] == "..":
-            tree.Insert("", f[4], str(q), f, icon=folder_icon)
-            q += 1
-            continue
-        else:
-            tree.Insert("", f[4], f[4], f, icon=folder_icon)
-            change_dir = f[7]
-            parent_key = f[4]
-            getdirdata(change_dir, parent_key)
-    else:
-        tree.Insert("", f[4], f[4], f, icon=file_icon)
-"""
-
-
+# Insert folder data to the treeview
 def insertfolder(data):
     list = [data[1], data[3], data[4], data[5], data[6], data[7], data[8], data[9], data[10], data[11], data[12]]
     tree.Insert(data[1], data[0], data[6], list)
 
 
+# Insert file data to the treeview
 def insertfile(data):
     list = [data[1], data[3], data[4], data[5], data[6], data[7], data[8], data[9], data[10], data[11], data[12]]
     tree.Insert(data[1], data[0], data[6], list, icon=file_icon)
 
 
+# Set the database
 db = lib.Sqlite.Sqlite(path=r"C:\Users\Gido Scherpenhuizen\Documents\,School\IIPFIT5\Project Files", filename="test")
 db.setup_database()
 
+# Start the image extraction proces
 lib.Image.start(db)
 
 # main
 database = db.get_files()
 
+# Add the data of every file and folder to the treeview
 for f in database:
     if f[12] == "DIR":
         list = [f[2], f[3], f[4], f[5], f[6], f[7], f[8], f[9], f[10], f[11], f[12]]
@@ -84,48 +66,7 @@ for f in database:
             tree.Insert(p_key[0], f[0], f[6], list, icon=file_icon)
 
 
-
-
-
-
-"""
-def folderinsert(key, display_text, value, p_key=""):
-    tree.Insert(p_key, randint(1, 1000000000), display_text, value, icon=folder_icon)
-
-
-def fileinsert(key, display_text, value, p_key=""):
-    tree.Insert(p_key, randint(1, 1000000000), display_text, value, icon=file_icon)
-
-
-def getdirectorydata(change_dir, parent_key):
-    for f in lib.Image.test.main("ImageUSBSjors.dd.001", "raw", change_dir):
-        if f[10] == "DIR":
-            changedir = change_dir
-            if f[4] == "." or f[4] == "..":
-                continue
-            else:
-                tree.Insert(parent_key, f[4], f[4], f, icon=folder_icon)
-                changedir = change_dir + "/" + f[4]
-                p_key = f[4]
-                getdirectorydata(changedir, p_key)
-        else:
-            tree.Insert(parent_key, f[4], f[4], f, icon=file_icon)
-
-
-# MAIN
-for f in lib.Image.test.main("ImageUSBSjors.dd.001", "raw"):
-    if f[10] == "DIR":
-        if f[4] != "." and f[4] != "..":
-            folderinsert(f[4], f[4], f)
-            change_dir = f[4]
-            parent_key = f[4]
-            getdirectorydata(change_dir, parent_key)
-        # else:
-            # folderinsert(f[4], f[4], randint(0, 100000), f)
-    else:
-        fileinsert(f[4], f[4], f, "")
-"""
-
+# Create a layout which is used for a window
 layout = [[Sg.Text('Welcome to Turtle Forensics!')],
           [treegoed]]
 
@@ -134,6 +75,7 @@ win1 = Sg.Window('TUF - Turtle Forensics').Layout(layout)
 
 win4_active = win3_active = win2_active = False
 
+# Turn on the window
 while True:
     ev1, vals1 = win1.Read()
     if ev1 == 'Properties':
