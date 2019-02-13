466bbbc88712fd53345f7a9e768b5a302f2d7edb
G1do11 <s1108069@student.hsleiden.nl>
2019-02-13 12:37:06 +0100

    YEEY WERKT

--- lib/Proef102.py
+++ lib/Proef102.py
@@ -5,11 +5,9 @@ import lib.Sqlite
 tree = Sg.TreeData()
 
 treegoed = Sg.Tree(data=tree,
-                   headings=['partition_id', 'file_md5', 'file_sha256', 'file_sha1', 'title', 'date_created',
-                             'date_last_modified', 'file_path', 'size', 'extension', 'file_type'],
+                   headings=['partition_id', 'file_path', 'size', 'extension', 'file_type'],
                    auto_size_columns=True,
-                   right_click_menu=['&Right', ['Extract', 'Upload to VirusTotal']],
-                   enable_events=True)
+                   right_click_menu=['&Right', ['Extract', 'Upload to VirusTotal']])
 
 folder_icon = b'iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAAACXBIWXMAAAsSAAALEgHS3X78AAABnUlEQVQ4y8WSv2rUQRSFv7vZ\
 gJFFsQg2EkWb4AvEJ8hqKVilSmFn3iNvIAp21oIW9haihBRKiqwElMVsIJjNrprsOr/5dyzml3UhEQIWHhjmcpn7zblw4B9lJ8Xag9mlmQb3AJzX3tOX8\
@@ -25,18 +23,6 @@ xGgemAGOHIBXxRjBWZMKoCPA2h6qEUSRR2MF6GxUUMUaIUgBCNTnAcm3H2G5YQfgvccYIXAtDH7FoKq/
 kAqRWy7+B7Z00G3xVc2wZeMSI4S7sVYkSk5Z/4PyBWROqvox3A28PN2cjUwinQC9QyckKALxj4kv2auK0xAAAAAElFTkSuQmCC'
 
 
-# Insert folder data to the treeview
-def insertfolder(data):
-    list = [data[1], data[3], data[4], data[5], data[6], data[7], data[8], data[9], data[10], data[11], data[12]]
-    tree.Insert(data[1], data[0], data[6], list)
-
-
-# Insert file data to the treeview
-def insertfile(data):
-    list = [data[1], data[3], data[4], data[5], data[6], data[7], data[8], data[9], data[10], data[11], data[12]]
-    tree.Insert(data[1], data[0], data[6], list, icon=file_icon)
-
-
 # Set the database
 db = lib.Sqlite.Sqlite(path=r"C:\Users\Gido Scherpenhuizen\Documents\,School\IIPFIT5\Project Files", filename="test")
 db.setup_database()
@@ -50,7 +36,7 @@ database = db.get_files()
 # Add the data of every file and folder to the treeview
 for f in database:
     if f[12] == "DIR":
-        list = [f[2], f[3], f[4], f[5], f[6], f[7], f[8], f[9], f[10], f[11], f[12]]
+        list = [f[2], f[9], f[10], f[11], f[12]]
         if f[1] == "":
             tree.Insert(f[1], f[0], f[6], list, icon=folder_icon)
         else:
@@ -58,7 +44,7 @@ for f in database:
             tree.Insert(p_key[0], f[0], f[6], list, icon=folder_icon)
 
     else:
-        list = [f[2], f[3], f[4], f[5], f[6], f[7], f[8], f[9], f[10], f[11], f[12]]
+        list = [f[2], f[9], f[10], f[11], f[12]]
         if f[1] == "":
             tree.Insert(f[1], f[0], f[6], list, icon=file_icon)
         else:
@@ -81,7 +67,7 @@ while True:
     if ev1 == 'Properties':
         win2_active = True
 
-    if ev1 == 'selected':
+    if vals1[0] == 'selected':
         print("CLICKED")
 
 win1.Close()
