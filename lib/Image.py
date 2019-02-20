f713c44b192f95c9ea1bb71ed2f7ecbd666b06ad
G1do11 <s1108069@student.hsleiden.nl>
2019-02-20 14:05:02 +0100

    YEEY WERKT 2.0

--- lib/Image.py
+++ lib/Image.py
@@ -3,7 +3,7 @@
 # Gido Scherpenhuizen #
 #      S1108069       #
 #######################
-
+##
 # Import libraries used for time conversion and easy path joins
 import datetime
 from os.path import join as pjoin
